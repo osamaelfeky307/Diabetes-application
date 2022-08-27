9e31d29c4be1ab91378fcedcf1576e63454d9ce9
osamaelfeky307 <osamaelfeky307@gmail.com>
2022-08-27 03:28:33 +0200

    edited text view in main activity

---
+++ app/src/main/java/com/example/splashscreen/splashScreenActivity.kt
@@ -0,0 +1,30 @@
+package com.example.splashscreen
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        val backgroundImg : ImageView = findViewById(R.id.iv_note)
+        val sideAnimation = AnimationUtils.loadAnimation(this,R.anim.slide)
+        backgroundImg.startAnimation(sideAnimation)
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this,MainActivity::class.java))
+                finish()
+            },3000
+        )
+
+
+    }
+
+
+}
